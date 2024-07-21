fd75c47837d4a1d9da6bd744c97813d9a656d9aa
AdityaK <hiraditya@msn.com>
2024-07-21 00:05:07 -0700

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,13 +15,13 @@ const About: React.FC = () => {
       <TextWrapper>
         <Title>ABOUT</Title>
         <Description>
-          IICT (Innovation In Compiler Technology) aimed at propagating the
-          innovations in compiler field invites researchers, practitioners, and
-          enthusiasts in the field of compiler technologies to submit
-          presentation proposals for our upcoming workshop. This year's theme
+          The IICT (Innovation In Compiler Technology) workshop aims to bring together
+          researchers, practitioners, and enthusiasts in the field of compiler technologies.
+          This year's theme
           focuses on the cutting-edge advancements in compiler design,
           implementation, and optimization, and their impact on emerging
           software and hardware platforms.
+          We invite presentation proposals for our upcoming workshop.
         </Description>
         <ButtonContainer>
           <Button onClick={toggleModal}>Get Updates</Button>
